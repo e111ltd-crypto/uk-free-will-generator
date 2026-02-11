78b95515c6468340c76a5b86a72ad8f7974da0d5
Duck <e111ltd@gmail.com>
2026-02-11 19:36:10 +0000

    Create Vite configuration file

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
